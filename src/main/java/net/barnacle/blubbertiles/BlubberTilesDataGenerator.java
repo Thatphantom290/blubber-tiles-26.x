e721a8995f8b35ac753e0e1b5149bc2b99c958cf
Thatphantom290 <thatphantomguthub@gmail.com>
2026-06-06 15:37:26 -0300

    Initial commit

---
+++ src/main/java/net/barnacle/blubbertiles/BlubberTilesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.barnacle.blubbertiles;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlubberTilesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
